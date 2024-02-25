3921ece205ff93c641d05cb437d81dc926ff22d0
imAshutoshGupta <aguptaworkspace@gmail.com>
2024-02-25 16:59:48 +0530

    conditional operators

---
+++ conditional.js
@@ -0,0 +1,16 @@
+//check given number is even or odd
+let a = prompt("enter a number")
+if (a % 2 == 0) {
+    console.log(`${a} is even`);
+    }
+else {
+    console.log(`${a} is odd`);
+}
+// check if given digit is two digit number
+let b = 658
+if(b>9 && b<100){
+    console.log(`${b} is two digit number`)
+}
+else {
+    console.log( `${b} is not two digit number`)
+}
