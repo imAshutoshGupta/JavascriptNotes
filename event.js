fba01f5d5a37cc6c7cb373beb4b5115877a1b3a1
imAshutoshGupta <aguptaworkspace@gmail.com>
2024-03-03 16:23:16 +0530

    objects

--- event.js
+++ event.js
@@ -36,14 +36,55 @@ console.log(x);
 emp = {...data6, data7}
 console.log(emp);
 
+//spread in function
+function add(...num){
+    console.log(num);
+    sum = 0
+    for(x of num){
+        sum+=x;
+    }
+    console.log(sum);
+}
+add(50,20,40,30,50,60,90)
 
+//rest operator
+let prices = [45,678,67]
+let [choco, coffee, samosa] = prices
+console.log(choco);
+console.log(coffee);
+console.log(samosa);
 
+console.log("----using rest----");
+let [carrot, ...fruits] = prices
+console.log(carrot);
+console.log(fruits);
 
+//rest on object
+let student = {name:"somya",age:45,city:"pune"}
+let {name,...details}=student
+console.log(name);
+console.log(details);
 
+//rest must be last
+function demo(a,...b){
+    console.log(a);
+    console.log(b);
+}
+demo(34,56,67,34,52,26)
 
+//working with objects
+let empy = {name:"raj", age:45, salary:98000, city:"bangalore"}
+function empy_details({name,salary}){
+    console.log(name);
+    console.log(salary);
+}
+empy_details(empy)
 
-
-
+// destructuring of array values
+let arr=[10,"hello","world","javascript"]
+let [w,p,,r]=arr;
+console.log(w);
+console.log(r);
 
 
 
